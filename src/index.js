1f6ec9bb233295d209be7080a4a890c03b394969
Corey Redding <coreysnightout@gmail.com>
2017-10-19 23:23:47 -0700

    build out video-list component to share state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCufEtZm1H16JIgHv64wstm2oKRjsbXRJ0';
 
 // Create a new component. this component should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // above is same as...this.setState({ videos: videos }) //
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // take this component's generated HTML and put it on the page (in the DOM)
